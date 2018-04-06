46784bdf05d4ce887b5b81cab64ebfc4910691c1
Igor Segota <igor@LONG3823.local>
2018-04-05 22:10:21 -0700

    Fix R folder

--- r/taxonomy.R
+++
@@ -1,9 +0,0 @@
-# Add taxonomy annotation to the final
-
-# test
-
-all_ab.dt = readRDS('~/cloud/research/microbiome/diabimmune/all_ab.dt_top4')
-blast_best.dt = readRDS('~/cloud/research/microbiome/diabimmune/blast_best.dt')
-
-var.dt = fread('~/cloud/research/microbiome/genomes/data/vregions_db/V3-V4_337F-805R_hang21_wrefseq_table.txt',
-               select=c())
