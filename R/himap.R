d80e7add29fd6d9c2042d724a3abcc9b243ac4e4
Igor Segota <is246@cornell.edu>
2018-04-10 15:38:44 -0700

    Testing how to retrieve inst files...

--- R/himap.R
+++ R/himap.R
@@ -57,8 +57,10 @@ assign('aln_params', c(5L, -4L, -8L, -6L), env=himap_opts)
 assign('ncpu', parallel::detectCores(), env=himap_opts)
 # BLAST databases
 assign('blast_dbs',
-       data.table::fread(system.file('inst', 'database', 'pcr_primers_table.txt',
-                   package='himap')),
+       data.table(
+         # system.file('inst', 'database', 'pcr_primers_table.txt', package='himap')
+         data('pcr_primers_table.txt', package='himap')
+       ),
        env=himap_opts)
 assign('blast_max_seqs', 2000, env=himap_opts)
 assign('blast_word_size', 13, env=himap_opts)
