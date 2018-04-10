0d0a616c1f901f284d22a1a12dbdb03bb87bef5c
Igor Segota <is246@cornell.edu>
2018-04-09 19:49:32 -0700

    Added detect_os() function.

--- R/himap.R
+++ R/himap.R
@@ -489,29 +489,3 @@ sequence_abundance = function (dada_result, remove_bimeras=T, collapse_sequences
   return(ab_mat_to_dt(ab_tab_nochim_coll, fq_prefix_split=fq_prefix_split))
 }
 
-
-#' Parse genus names and strain counts from each OSU species string
-#'
-osuab_genuses = function (osuab) {
-  osuab[, {
-    species_list = strsplit(species[1], ',', fixed=T)[[1]]
-    genus_counts = lapply(species_list, function (s) {
-      genus = gsub('^([^_]+)_.*', '\\1', s)
-      strain_count = gsub('.*_\\[([0-9]+)\\]$', '\\1', s)
-      if (strain_count == s) strain_count = '1'
-      return(c(genus, strain_count))
-    })
-    genus_counts_unique = list()
-    for (i in 1:length(genus_counts)) {
-      genus_i = genus_counts[[i]][1]
-      counts_i = as.integer(genus_counts[[i]][2])
-      if (genus_i %in% names(genus_counts_unique)) {
-        genus_counts_unique[[genus_i]] = genus_counts_unique[[genus_i]] + counts_i
-      } else {
-        genus_counts_unique[[genus_i]] = counts_i
-      }
-    }
-    # Now pool together same genuses and add up the counts
-    list('genus'=names(genus_counts_unique), 'genus_strain_count'=unname(genus_counts_unique))
-  }, by=osu_id]
-}
